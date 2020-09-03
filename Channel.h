936a411acd785a8eb918bec9aac99d448031be34
bugken <yanzhenwei2011@163.com>
2020-09-03 14:09:15 +0800

    完善Channel头文件，增加相应的成员函数

--- Channel.h
+++ Channel.h
@@ -8,6 +8,7 @@
 
 class EventLoop;
 /*
+*理清楚 EventLoop Channel Poller之间的关系 <- Reactor模型上对应Demultiplex
 *Channel理解为通道，封装了sockfd和其感兴趣的event，如EPOLLIN、EPLLOUT事件
 *还绑定了poller返回的具体事件
 */
@@ -20,13 +21,43 @@ public:
     Channel(EventLoop* loop, int fd);
     ~Channel();
 
+    //fd得到poller通知以后，处理事件
     void handleEvent(Timestamp receiveTime);
+    //设置回调函数
     void setReadCallback(ReadEventCallback cb){ readCallback_ = std::move(cb); }
     void setWriteCallback(EventCallback cb){ writeCallback_ = std::move(cb); }
     void setCloseCallback(EventCallback cb){ closeCallback_ = std::move(cb); }
     void setErrorCallback(EventCallback cb){ errorCallback_ = std::move(cb); }
 
+    //防止Channel被手动remove掉,Channel还在执行回调操作
+    void tie(const std::shared_ptr<void>&);
+
+    int fd() const{ return fd_; }
+    int events() const{ return events_; }
+    int set_revents(int revt){ revents_ = revt; }
+    bool isNoneEvent() const{ return events_ == kNoneEvent; }
+
+    //设置fd相应的事件状态
+    void enableReading(){ events_ |= kReadEvent; update(); }
+    void disableReading(){ events_ &= ~kReadEvent; update(); }
+    void enableWriting(){ events_ |= kWriteEvent; update(); }
+    void disableWriting(){ events_ &= ~kWriteEvent; update(); }
+    void disableAll(){ events_ = kNoneEvent; update(); }
+
+    //返回fd当前的事件状态
+    bool isWriting() const { return events_ & kWriteEvent; }
+    bool isReading() const { return events_ & kReadEvent; }
+
+    int index() { return index_; }
+    void set_index(int idx) { index_ = idx; }
+
+    EventLoop* ownerLoop() { return loop_; }
+    void remove();
+
 private:
+    void update();
+    void handleEventWithGuard(Timestamp receiveTime);
+
     static const int kNoneEvent;
     static const int kReadEvent;
     static const int kWriteEvent;
