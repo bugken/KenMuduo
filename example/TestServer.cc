f5854f5660c140963bb0e4f71a80cc2f06d19ed0
bugken <yanzhenwei2011@163.com>
2020-09-08 11:47:17 +0800

    去掉打印信息

--- example/TestServer.cc
+++ example/TestServer.cc
@@ -10,20 +10,16 @@ public:
     EchoServer(EventLoop* loop, const InetAddress& addr, const std::string& name)
         :server_(loop_, addr, name), loop_(loop)
     {
-        LOG_INFO("%s %s %d\n", __FILENAME__, __FUNCTION__, __LINE__);
         //注册回调函数
         server_.setConnectionCallback(std::bind(&EchoServer::onConnection, this, std::placeholders::_1));
         server_.setMessageCallback(std::bind(&EchoServer::onMessage, this, 
                     std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
         //设置合适的loop线程数量
         server_.setThreadNum(1);
-        LOG_INFO("%s %s %d\n", __FILENAME__, __FUNCTION__, __LINE__);
     }
     void start()
     {
-        LOG_INFO("%s %s %d\n", __FILENAME__, __FUNCTION__, __LINE__);
         server_.start();
-        LOG_INFO("%s %s %d\n", __FILENAME__, __FUNCTION__, __LINE__);
     }
 private:
     //连接建立或者断开回调函数
@@ -58,6 +54,5 @@ int main()
     EchoServer server(&loop, addr, "EchoServer");
     server.start();
     loop.loop();
-    LOG_INFO("%s %s %d\n", __FILENAME__, __FUNCTION__, __LINE__);
     return 0;
 }
